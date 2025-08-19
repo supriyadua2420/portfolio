4e7ecda75fa1d853d4e492a7fd9df7e36ecfab27
Supriya Dua <supriya@hiringbranch.com>
2025-08-19 01:26:15 -0700

    add email id

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
       <h2 className="text-3xl font-bold text-pink-500 mb-2">Get in Touch</h2>
       <p className="text-gray-600 mb-6">Feel free to reach out for collaborations or just a friendly hello</p>
       <a
-      href="mailto:youremail@example.com"
+      href="mailto:supriyadua24@gmail.com"
       className="px-6 py-3 bg-pink-300 text-white font-semibold rounded-full shadow-md hover:bg-pink-400 hover:shadow-lg hover:scale-105 transition-all duration-300 inline-block">
       Email Me
       </a>
